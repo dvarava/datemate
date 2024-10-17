fac6c479fbf2f52f32361272028eb40b5d9aba1a
denielvarava <varavadenik16@gmail.com>
2024-10-17 13:06:11 +0100

    responsive PremiumScreen

--- frontend/src/app/navigation/premium/index.tsx
+++ frontend/src/app/navigation/premium/index.tsx
@@ -8,6 +8,7 @@ import {
   TextStyle,
   Image,
   ImageStyle,
+  ScrollView,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
@@ -35,34 +36,51 @@ const PremiumScreen = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.logoContainer}>
-          <Image
-            source={require("assets/gold-logo.png")}
-            style={styles.logoImage}
-          />
+      <ScrollView style={styles.scrollContainer} contentContainerStyle={styles.scrollContent}>
+        <View style={styles.header}>
+          <View style={styles.logoContainer}>
+            <Image
+              source={require("assets/gold-logo.png")}
+              style={styles.logoImage}
+            />
+          </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>DateMate</Text>
+            <GradientText text="PREMIUM" style={styles.premiumText} />
+          </View>
         </View>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>DateMate</Text>
-          <GradientText text="PREMIUM" style={styles.premiumText} />
-        </View>
-      </View>
 
-      <View style={styles.plansContainer}>
-        <TouchableOpacity onPress={() => handleSelectPlan("yearly")}>
-          <View style={styles.planCardWrapper}>
-            {selectedPlan === "yearly" ? (
-              <GradientBorder
-                borderRadius={15}
-                colors={[
-                  colors.premium1,
-                  colors.premium2,
-                  colors.premium3,
-                  colors.premium4,
-                ]}
-                start={{ x: 0.25, y: 0.3 }}
-                end={{ x: 1, y: 1 }}
-              >
+        <View style={styles.plansContainer}>
+          {/* Yearly Plan */}
+          <TouchableOpacity onPress={() => handleSelectPlan("yearly")}>
+            <View style={styles.planCardWrapper}>
+              {selectedPlan === "yearly" ? (
+                <GradientBorder
+                  borderRadius={15}
+                  colors={[
+                    colors.premium1,
+                    colors.premium2,
+                    colors.premium3,
+                    colors.premium4,
+                  ]}
+                  start={{ x: 0.25, y: 0.3 }}
+                  end={{ x: 1, y: 1 }}
+                >
+                  <View style={styles.planCard}>
+                    <View style={styles.planDetailsContainer}>
+                      <Text style={styles.planTitle}>12 Months</Text>
+                      <View style={styles.oldPriceContainer}>
+                        <Text style={styles.oldPrice}>$60</Text>
+                        <Text style={styles.discountedPrice}>$45</Text>
+                      </View>
+                    </View>
+
+                    <View style={styles.monthlyPriceContainer}>
+                      <Text style={styles.monthlyPrice}>$3.75 / month</Text>
+                    </View>
+                  </View>
+                </GradientBorder>
+              ) : (
                 <View style={styles.planCard}>
                   <View style={styles.planDetailsContainer}>
                     <Text style={styles.planTitle}>12 Months</Text>
@@ -76,47 +94,47 @@ const PremiumScreen = () => {
                     <Text style={styles.monthlyPrice}>$3.75 / month</Text>
                   </View>
                 </View>
-              </GradientBorder>
-            ) : (
-              <View style={styles.planCard}>
-                <View style={styles.planDetailsContainer}>
-                  <Text style={styles.planTitle}>12 Months</Text>
-                  <View style={styles.oldPriceContainer}>
-                    <Text style={styles.oldPrice}>$60</Text>
-                    <Text style={styles.discountedPrice}>$45</Text>
+              )}
+
+              <LinearGradient
+                colors={["#000000", "#202020", "#252525"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                style={styles.mostPopularBadge}
+              >
+                <Text style={styles.mostPopularText}>Most popular</Text>
+              </LinearGradient>
+            </View>
+          </TouchableOpacity>
+
+          {/* Quarterly Plan */}
+          <TouchableOpacity onPress={() => handleSelectPlan("quarterly")}>
+            {selectedPlan === "quarterly" ? (
+              <GradientBorder
+                borderRadius={15}
+                colors={[
+                  colors.premium1,
+                  colors.premium2,
+                  colors.premium3,
+                  colors.premium4,
+                ]}
+                start={{ x: 0.25, y: 0.3 }}
+                end={{ x: 1, y: 1 }}
+              >
+                <View style={styles.planCard}>
+                  <View style={styles.planDetailsContainer}>
+                    <Text style={styles.planTitle}>3 Months</Text>
+                    <View style={styles.oldPriceContainer}>
+                      <Text style={styles.discountedPrice}>$24.99</Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.monthlyPriceContainer}>
+                    <Text style={styles.monthlyPrice}>$7.99 / month</Text>
                   </View>
                 </View>
-
-                <View style={styles.monthlyPriceContainer}>
-                  <Text style={styles.monthlyPrice}>$3.75 / month</Text>
-                </View>
-              </View>
-            )}
-
-            <LinearGradient
-              colors={["#000000", "#202020", "#252525"]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 1 }}
-              style={styles.mostPopularBadge}
-            >
-              <Text style={styles.mostPopularText}>Most popular</Text>
-            </LinearGradient>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => handleSelectPlan("quarterly")}>
-          {selectedPlan === "quarterly" ? (
-            <GradientBorder
-              borderRadius={15}
-              colors={[
-                colors.premium1,
-                colors.premium2,
-                colors.premium3,
-                colors.premium4,
-              ]}
-              start={{ x: 0.25, y: 0.3 }}
-              end={{ x: 1, y: 1 }}
-            >
+              </GradientBorder>
+            ) : (
               <View style={styles.planCard}>
                 <View style={styles.planDetailsContainer}>
                   <Text style={styles.planTitle}>3 Months</Text>
@@ -129,93 +147,85 @@ const PremiumScreen = () => {
                   <Text style={styles.monthlyPrice}>$7.99 / month</Text>
                 </View>
               </View>
-            </GradientBorder>
-          ) : (
-            <View style={styles.planCard}>
-              <View style={styles.planDetailsContainer}>
-                <Text style={styles.planTitle}>3 Months</Text>
-                <View style={styles.oldPriceContainer}>
-                  <Text style={styles.discountedPrice}>$24.99</Text>
-                </View>
-              </View>
+            )}
+          </TouchableOpacity>
+        </View>
 
-              <View style={styles.monthlyPriceContainer}>
-                <Text style={styles.monthlyPrice}>$7.99 / month</Text>
-              </View>
-            </View>
-          )}
-        </TouchableOpacity>
-      </View>
+        <GradientText
+          text="WITH YEARLY PLAN YOU SAVE 30%"
+          style={styles.savingsText}
+        />
 
-      <GradientText
-        text="WITH YEARLY PLAN YOU SAVE 30%"
-        style={styles.savingsText}
-      />
-      <View style={styles.featureCard}>
-        <GradientBorder
-          borderRadius={20}
-          colors={[
-            colors.premium1,
-            colors.premium2,
-            colors.premium3,
-            colors.premium4,
-          ]}
-          start={{ x: 0.25, y: 0.3 }}
-          end={{ x: 1, y: 1 }}
+        {/* Features Section */}
+        <View style={styles.featureCard}>
+          <GradientBorder
+            borderRadius={20}
+            colors={[
+              colors.premium1,
+              colors.premium2,
+              colors.premium3,
+              colors.premium4,
+            ]}
+            start={{ x: 0.25, y: 0.3 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <LinearGradient
+              colors={["#000000", "#252525", "#000000", "#252525"]}
+              locations={[0, 0.25, 0.5, 1]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 2, y: 0 }}
+              style={styles.featureCardContentWrapper}
+            >
+              <View style={styles.featureCardContent}>
+                <FeatureItem
+                  icon="cash-outline"
+                  text="Set budget for the date."
+                />
+                <FeatureItem
+                  icon="happy-outline"
+                  text="Set the mood for the date."
+                />
+                <FeatureItem icon="cloud-outline" text="Adjust to the weather." />
+                <FeatureItem
+                  icon="speedometer-outline"
+                  text="Increased date generations."
+                />
+                <FeatureItem
+                  icon="gift-outline"
+                  text="Tailored gift recommendation."
+                />
+                <FeatureItem
+                  icon="time-outline"
+                  text="Select preferred date time."
+                />
+                <FeatureItem icon="timer-outline" text="Set date duration." />
+              </View>
+            </LinearGradient>
+          </GradientBorder>
+        </View>
+      </ScrollView>
+
+      {/* Continue Button Section */}
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity
+          onPress={handleProceedToPurchase}
+          style={styles.fullButton}
         >
           <LinearGradient
-            colors={["#000000", "#252525", "#000000", "#252525"]}
-            locations={[0, 0.25, 0.5, 1]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 2, y: 0 }}
-            style={styles.featureCardContentWrapper}
+            colors={[
+              colors.premium1,
+              colors.premium2,
+              colors.premium3,
+              colors.premium4,
+            ]}
+            start={{ x: 0.25, y: 0.3 }}
+            end={{ x: 1, y: 1 }}
+            style={styles.continueButton}
           >
-            <View style={styles.featureCardContent}>
-              <FeatureItem
-                icon="cash-outline"
-                text="Set budget for the date."
-              />
-              <FeatureItem
-                icon="happy-outline"
-                text="Set the mood for the date."
-              />
-              <FeatureItem icon="cloud-outline" text="Adjust to the weather." />
-              <FeatureItem
-                icon="speedometer-outline"
-                text="Increased date generations."
-              />
-              <FeatureItem
-                icon="gift-outline"
-                text="Tailored gift recommendation."
-              />
-              <FeatureItem
-                icon="time-outline"
-                text="Select preferred date time."
-              />
-              <FeatureItem icon="timer-outline" text="Set date duration." />
-            </View>
+            <Text style={styles.continueButtonText}>CONTINUE</Text>
           </LinearGradient>
-        </GradientBorder>
+        </TouchableOpacity>
       </View>
-
-      <TouchableOpacity
-        onPress={handleProceedToPurchase}
-        style={styles.fullButton}
-      >
-        <LinearGradient
-          colors={[
-            colors.premium1,
-            colors.premium2,
-            colors.premium3,
-            colors.premium4,
-          ]}
-          start={{ x: 0.25, y: 0.3 }}
-          end={{ x: 1, y: 1 }}
-          style={styles.continueButton}
-        >
-          <Text style={styles.continueButtonText}>CONTINUE</Text>
-        </LinearGradient>
-      </TouchableOpacity>
     </View>
   );
 };
@@ -237,12 +247,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
-    padding: 20,
+    justifyContent: "space-between",
+  } as ViewStyle,
+  scrollContainer: {
+    flex: 1,
+  } as ViewStyle,
+  scrollContent: {
+    paddingHorizontal: 20,
   } as ViewStyle,
   header: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 20,
+    marginBottom: 10,
+    marginTop: 10,
     justifyContent: "center",
   } as ViewStyle,
   logoContainer: {
@@ -338,7 +355,7 @@ const styles = StyleSheet.create({
   savingsText: {
     textAlign: "center",
     color: colors.premium2,
-    marginBottom: 40,
+    marginBottom: 30,
     fontFamily: "Nunito-Bold",
     fontSize: 11,
   } as TextStyle,
@@ -359,13 +376,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   } as ViewStyle,
-  iconGradient: {
-    width: 32,
-    height: 32,
-    borderRadius: 16,
-    justifyContent: "center",
-    alignItems: "center",
-  } as ViewStyle,
   gradientIcon: {
     color: "white",
   } as TextStyle,
@@ -375,6 +385,10 @@ const styles = StyleSheet.create({
     color: "white",
     fontFamily: "Nunito-Regular",
   } as TextStyle,
+  buttonWrapper: {
+    paddingHorizontal: 20,
+    paddingBottom: 30,
+  } as ViewStyle,
   fullButton: {
     borderRadius: 25,
     overflow: "hidden",
@@ -383,7 +397,6 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     alignItems: "center",
     paddingVertical: 15,
-    marginTop: 40,
   } as ViewStyle,
   continueButtonText: {
     fontSize: 16,
